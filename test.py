98b3ee5e500a12acca411cf85622e26d7577b756
Yuna Oh <ustina0409@gmail.com>
2021-06-06 13:21:47 -0700

    added tests for multiply

--- test.py
+++ test.py
@@ -10,6 +10,9 @@ class TestCase(unittest.TestCase):
     def test_subtract_1(self):
         self.assertEqual(example.subtract(5, 2), 3)
 
+    def test_multiply_1(self):
+        self.assertEqual(example.multiply(1, 3), 3)
+
 
 if __name__ == '__main__':
     unittest.main()
